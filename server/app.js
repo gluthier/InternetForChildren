2e79228c25ae8439aa9e5ab1e4dbb6a3683c2eb9
Gabriel Luthier <gabluthier@gmail.com>
2015-11-20 07:48:43 -0800

    Server works

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var crypto = require('crypto');
-//var phashLib = require('phash'); // TODO
+var phash = require('phash-image');
 var request = require('request');
+var randomstring = require("randomstring");
+var md5File = require('md5-file');
+var fs = require('fs');
+
 var Schema = mongoose.Schema;
 
 var imageSchema = new Schema({
@@ -45,11 +49,13 @@ app.post('/url/check', function(req, res) {
         if (done) res.json({ blocked: true });
       });
     } else {
-      request.get(url, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var algo = 'md5';
-            var shasum = crypto.createHash(algo);
-            var h = shasum.update(body).digest('hex');
+      var name  = "/tmp/" + randomstring.generate() + "." + url.split('.').pop();
+      var stream = fs.createWriteStream(name);
+      stream.on('close', function() {
+        stream.close();
+        md5File(name, function (error, h) {
+          if (error) return handleError(error, res);
+          else {
             Image.findOne({md5s: h}, function(err, imgMd5) {
               if (err) {
                 return handleError(err, res);
@@ -63,29 +69,36 @@ app.post('/url/check', function(req, res) {
                   if (done) res.json({blocked: true});
                 });
               } else {
-                var phashPic = phashLib.compute(picture); // TODO
-
-                Image.find({phash: phashPic}, function(err, imgPh) {
-                  if (err) {
-                    return handleError(err, res);
-                  }
-                  if (imgPh) {
-                    imgPh.urls.push(url);
-                    imgPh.md5s.push(h);
-                    imgPh.nView = imgPh.nView + 1;
-                    var ratio = computeRatio(imgPh);
-                    imgPh.save(function(err, done) {
-                      if (err) return handleError(err, res);
-                      if (done) res.json({blocked: true});
+                phash(name, function(err, pHash) {
+                  if(err) return handleError(err, res);
+                  else {
+                    Image.findOne({phash: pHash}, function(err, imgPh) {
+                      if (err) {
+                        return handleError(err, res);
+                      }
+                      if (imgPh) {
+                        imgPh.urls.push(url);
+                        imgPh.md5s.push(h);
+                        imgPh.nView = imgPh.nView + 1;
+                        var ratio = computeRatio(imgPh);
+                        imgPh.save(function(err, done) {
+                          if (err) return handleError(err, res);
+                          if (done) res.json({blocked: true});
+                        });
+                      } else {
+                        res.json({blocked: false});
+                      }
                     });
-                  } else {
-                    res.json({blocked: false});
                   }
                 });
               }
             });
           }
+        })
       });
+      request.get({uri: url})
+      .pipe(stream);
+
     }
   });
 });
@@ -96,7 +109,8 @@ app.post('/url/block/', function(req, res) {
     return res.sendStatus(400);
   }
 
-  Image.findOne({ urls: req.body.url }, function(err, img) {
+  var url = req.body.url;
+  Image.findOne({ urls: url }, function(err, img) {
     if (err) return handleError(err, res);
     if (img) {
       img.nReport = img.nReport + 1;
@@ -108,31 +122,66 @@ app.post('/url/block/', function(req, res) {
 
       // TODO add image
       // Compute ph and look for it
-
-      // If PH not found, we add a new image
-      request.get(req.body.url, function (error, response, body)  {
-          if (!error && response.statusCode == 200) {
-              var algo = 'md5';
-              var shasum = crypto.createHash(algo);
-              var h = shasum.update(body).digest('hex');
-              var img = new Image({
-                phash: "trololo", // TODO change
-                md5s: [h],
-                urls: [req.body.url],
-                nView         : 1,
-                nReport       : 1,
-                nAllow        : 0
-              });
-              var done = img.save(function(err, model) {
-                if (err) {
-                  return handleError(err, res);
-                }
-                if (done) {
-                  return res.sendStatus(200); // TODO
-                }
-              });
+      var name  = "/tmp/" + randomstring.generate() + "." + url.split('.').pop();
+      var stream = fs.createWriteStream(name);
+      stream.on('close', function() {
+        stream.close();
+        md5File(name, function (error, h) {
+          if (error) return handleError(error, res);
+          else {
+            Image.findOne({md5s: h}, function(err, imgMd5) {
+              if (err) {
+                return handleError(err, res);
+              }
+              if (imgMd5) {
+                var ratio = computeRatio(imgMd5);
+                imgMd5.nView = imgMd5.nView + 1;
+                imgMd5.urls.push(url);
+                var done = imgMd5.save(function(err, done) {
+                  if (err) return handleError(err, res);
+                  if (done) res.sendStatus(200);
+                });
+              } else {
+                phash(name, function(err, pHash) {
+                  if(err) return handleError(err, res);
+                  else {
+                    Image.findOne({phash: pHash}, function(err, imgPh) {
+                      if (err) {
+                        return handleError(err, res);
+                      }
+                      if (imgPh) {
+                        imgPh.urls.push(url);
+                        imgPh.md5s.push(h);
+                        imgPh.nView = imgPh.nView + 1;
+                        var ratio = computeRatio(imgPh);
+                        imgPh.save(function(err, done) {
+                          if (err) return handleError(err, res);
+                          if (done) res.sendStatus(200);
+                        });
+                      } else {
+                        var image = new Image({
+                          phash: pHash,
+                          md5s: [h],
+                          urls: [url],
+                          nView         : 1,
+                          nReport       : 1,
+                          nAllow        : 0
+                        });
+                        image.save(function(err, done) {
+                          if(err) return handleError(err, res);
+                          else res.sendStatus(200);
+                        })
+                      }
+                    });
+                  }
+                });
+              }
+            });
           }
+        })
       });
+      request.get({uri: url})
+      .pipe(stream);
     }
   });
 });
@@ -151,7 +200,7 @@ app.post('/url/unblock/', function(req, res) {
         if (done) res.sendStatus(200);
       });
     } else {
-        res.sendStatus(200);
+      res.sendStatus(200);
     }
   });
 });
