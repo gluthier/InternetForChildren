0ef958d8844d8a5f32a69ca2195f32c29ca213b9
ex0ns <guillaume.tournigand@epfl.ch>
2015-11-20 00:07:50 -0800

    Fix merge issue

--- extension/index.js
+++ extension/index.js
@@ -5,6 +5,6 @@ pageMod.PageMod({
     contentScriptWhen: 'ready',
     contentScriptFile: [data.url("jquery.js"),
                         data.url("wfk.js"),
+                        data.url("md5.js"),
                         data.url("contentScript.js")]
 });
->>>>>>> page-mod
